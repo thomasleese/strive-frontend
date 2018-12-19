0900acfaaf881a55751dcd94793d3f8e3d20d530
vikash tank <vikashtank@hotmail.co.uk>
2018-12-19 18:41:41 +0000

    add iphone image to pane

--- healthy-gym/src/components/IntroPane/IntroPane.js
+++ healthy-gym/src/components/IntroPane/IntroPane.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import backgroundImage from './images/app_background.jpg';
+import iphoneImage from './images/iphone_cover_dark.png';
 
 
 function IntroPane(props) {
 
   var style = {
     backgroundImage: "url(" + backgroundImage + ")",
-    backgroundPosition: '50% 50%'
+    backgroundPosition: '50% 50%',
   }
 
   return (
@@ -14,8 +15,8 @@ function IntroPane(props) {
       <div className="container mt-5 pt-5">
         <div className="row">
           <div className="col-md-6 my-5 text-lg-left text-center align-self-center">
-            <h1 className="display-2">App Intro</h1>
-            <p className="lead">Pingendo for apps and techies</p>
+            <h1 className="display-2 text-light">Healthy Gym</h1>
+            <p className="lead text-light">A helpful app for tracking workout progress</p>
             <div className="row mt-5">
               <div className="col-6 col-lg-4">
                 <a href="/">
@@ -30,7 +31,7 @@ function IntroPane(props) {
             </div>
           </div>
           <div className="col-lg-6">
-            <img className="img-fluid d-block mx-auto" src="assets/app/iphone_cover_dark.png" width="400"/>
+            <img className="img-fluid d-block mx-auto" src={iphoneImage} width="400"/>
           </div>
         </div>
       </div>
