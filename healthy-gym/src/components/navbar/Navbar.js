5101c6fddbb5237699150df668b20b9f09748d04
vikash tank <vikashtank@hotmail.co.uk>
2018-12-19 17:47:29 +0000

    fix css

--- healthy-gym/src/components/navbar/Navbar.js
+++ healthy-gym/src/components/navbar/Navbar.js
@@ -5,20 +5,20 @@ function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
       <div className="container">
-        <h1 className="navbar-brand" >Healthy Gym</h1>
+        <a className="navbar-brand" href="/" >Healthy Gym</a>
         <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
           <ul className="navbar-nav">
             <li className="nav-item mx-2">
-              <h1 className="nav-link">Features</h1>
+              <a className="nav-link" href="/">Features</a>
             </li>
             <li className="nav-item mx-2">
-              <h1 className="nav-link">Reviews</h1>
+              <a className="nav-link" href="/">Reviews</a>
             </li>
           </ul>
-          <h1 className="btn navbar-btn mx-2 btn-primary shadowed" >Download</h1>
+          <a className="btn navbar-btn mx-2 btn-primary shadowed" href="/">Download</a>
         </div>
       </div>
     </nav>
