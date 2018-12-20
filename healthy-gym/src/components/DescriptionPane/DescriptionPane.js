8113cde843917543595ae31fc0c7e3e0e556ada3
vikash tank <vikashtank@hotmail.co.uk>
2018-12-20 15:18:18 +0000

    give images alt prop

--- healthy-gym/src/components/DescriptionPane/DescriptionPane.js
+++ healthy-gym/src/components/DescriptionPane/DescriptionPane.js
@@ -8,7 +8,7 @@ function AboutSubpane(props) {
   return (
     <div className="row py-5">
       <div className="col-md-5 order-2 order-md-1 animate-in-left">
-        <img className="img-fluid d-block my-3 mx-auto" src={iphoneIsometric} width="200"/>
+        <img className="img-fluid d-block my-3 mx-auto" src={iphoneIsometric} width="200" alt=""/>
       </div>
       <div className="col-md-7 align-self-center order-1 order-md-2 my-3 text-md-left text-center">
         <h2>Simple, Helpful, and Productive</h2>
@@ -37,7 +37,7 @@ function SupportSubpane(props) {
         <a className="btn btn-lg btn-outline-primary shadowed m-1" href="/">Twitter</a>
       </div>
       <div className="align-self-center mt-5 col-lg-5 animate-in-right">
-        <img className="img-fluid d-block" src={iphoneIpadImage}/> </div>
+        <img className="img-fluid d-block" src={iphoneIpadImage} alt=""/> </div>
     </div>
   )
 }
