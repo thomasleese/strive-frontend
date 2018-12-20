a5dcbe5cf26e9918cb3280d58c57ff017a5b0e5c
vikash tank <vikashtank@hotmail.co.uk>
2018-12-20 17:07:12 +0000

    change image in pane

--- healthy-gym/src/components/ActionPane/ActionPane.js
+++ healthy-gym/src/components/ActionPane/ActionPane.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import IOSButton from './../Buttons/Download/IOSButton';
 import isometricImage from './images/iphone_isometric_dark.png';
-import runningImage from './images/running.jpg';
+import runningImage from './images/barbell.jpg';
 
 function ActionPane(props) {
 
@@ -9,16 +9,16 @@ function ActionPane(props) {
      backgroundImage: "url(" + runningImage + ")",
      backgroundPosition: 'top left',
      height: '500px',
+     backgroundSize: '100%',
   }
 
   return (
     <div className="py-5 bg-primary" id="download" style={style}>
       <div className="container">
         <div className="row">
-          <div className="col-md-6 align-self-center text-center text-md-left"></div>
           <div className="col-md-6 align-self-center text-center text-md-left">
-            <h1><b>Download Healthy Gym</b></h1>
-            <p className="lead text-dark"><b className="text-dark">Get it on your iOS device right now</b></p>
+            <h1 className='text-light'><b>Download Healthy Gym</b></h1>
+            <p className="lead text-light"><b className="text-light">Get it on your iOS device right now</b></p>
             <div className="row mt-5">
               <div className="col-6 col-lg-4">
                 <IOSButton />
