ac7329279a864328aa0b428d111be81cd392ac51
vikash tank <vikashtank@hotmail.co.uk>
2018-12-20 17:18:46 +0000

    add footer

---
+++ healthy-gym/src/components/Footer/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Footer(props) {
+
+  return (
+    <div className="py-5 bg-dark">
+      <div className="container">
+        <div className="row">
+          <div className="col-4 col-md-1 align-self-center my-3">
+            <a href="https://www.facebook.com" target="blank"><i className="fa fa-fw fa-facebook fa-3x text-white"></i></a>
+          </div>
+          <div className="col-4 col-md-1 align-self-center my-3">
+            <a href="https://twitter.com" target="blank"><i className="fa fa-fw fa-twitter fa-3x text-white"></i></a>
+          </div>
+          <div className="col-4 col-md-1 align-self-center my-3">
+            <a href="https://www.instagram.com" target="blank"><i className="fa fa-fw fa-medium fa-3x text-white"></i></a>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 my-3 text-center">
+            <p className="text-muted">© Copyright 2019 Orycion - All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
