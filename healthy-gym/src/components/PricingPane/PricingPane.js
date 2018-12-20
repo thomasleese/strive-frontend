6431bb6fafc75fa76fdd4a31844b8638b127a0c1
vikash tank <vikashtank@hotmail.co.uk>
2018-12-20 13:33:41 +0000

    replace single with double quotes

--- healthy-gym/src/components/PricingPane/PricingPane.js
+++ healthy-gym/src/components/PricingPane/PricingPane.js
@@ -23,20 +23,20 @@ function PricingPane(props) {
         <div className="row">
           <div className="col-sm-6">
             <Card title="Free Package" linkText="Download Now">
-              <ul className='list-group text-left'>
-                <li className='list-group-item'>Unlimited Workouts</li>
-                <li className='list-group-item'>3 Workout Templates</li>
+              <ul className="list-group text-left">
+                <li className="list-group-item">Unlimited Workouts</li>
+                <li className="list-group-item">3 Workout Templates</li>
               </ul>
             </Card>
           </div>
           <div className="col-sm-6">
             <Card title="Goal Package" linkText="Download Now" subtitle='Only £5.00 a year'>
-              <ul className='list-group text-left'>
-                <li className='list-group-item'>Unlimited Workouts</li>
-                <li className='list-group-item'>Unlimited Workout Templates</li>
-                <li className='list-group-item'>Add Goals</li>
-                <li className='list-group-item'>Progress Charts</li>
-                <li className='list-group-item'>Suggested Sets</li>
+              <ul className="list-group text-left">
+                <li className="list-group-item">Unlimited Workouts</li>
+                <li className="list-group-item">Unlimited Workout Templates</li>
+                <li className="list-group-item">Add Goals</li>
+                <li className="list-group-item">Progress Charts</li>
+                <li className="list-group-item">Suggested Sets</li>
               </ul>
             </Card>
           </div>
