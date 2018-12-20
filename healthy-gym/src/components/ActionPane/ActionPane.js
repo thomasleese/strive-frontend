255b72c0c1afe234479c417bb10318c0660fa1f1
vikash tank <vikashtank@hotmail.co.uk>
2018-12-20 16:49:57 +0000

    add background image to action pane

--- healthy-gym/src/components/ActionPane/ActionPane.js
+++ healthy-gym/src/components/ActionPane/ActionPane.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import IOSButton from './../Buttons/Download/IOSButton';
-import isometricImage from './images/iphone_isometric_dark.png'
+import isometricImage from './images/iphone_isometric_dark.png';
+import runningImage from './images/running.jpg';
 
 function ActionPane(props) {
 
+  let style = {
+     backgroundImage: "url(" + runningImage + ")",
+     backgroundPosition: 'top left',
+     height: '500px',
+  }
+
   return (
-    <div className="py-5 bg-primary" id="download">
+    <div className="py-5 bg-primary" id="download" style={style}>
       <div className="container">
         <div className="row">
-          <div className="col-md-6 animate-in-left">
-            <img className="img-fluid d-block my-4 animate-slide-in" src={isometricImage} /> </div>
+          <div className="col-md-6 align-self-center text-center text-md-left"></div>
           <div className="col-md-6 align-self-center text-center text-md-left">
             <h1><b>Download Healthy Gym</b></h1>
-            <p className="lead text-dark"><b className="text-white">Get it on your iOS device right now</b></p>
+            <p className="lead text-dark"><b className="text-dark">Get it on your iOS device right now</b></p>
             <div className="row mt-5">
               <div className="col-6 col-lg-4">
                 <IOSButton />
