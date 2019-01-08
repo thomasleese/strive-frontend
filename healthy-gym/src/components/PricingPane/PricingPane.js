7dad2864741774559e318794df8d0503e27618f2
vikash tank <vikashtank@hotmail.co.uk>
2019-01-08 20:52:52 +0000

    scale pricing cards

--- healthy-gym/src/components/PricingPane/PricingPane.js
+++ healthy-gym/src/components/PricingPane/PricingPane.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import CardRow from './../CardRow/CardRow';
+
 
 function Card(props) {
   return (
-    <div className="card">
+    <div className=' card border border-primary p-3 m-3 animate-in-down align-items-stretch align-content-stretch w-100'>
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
         <span className='text-muted mb1'>{props.subtitle}</span>
@@ -20,27 +22,23 @@ function PricingPane(props) {
   return (
     <div className="py-5 bg-primary" id="download">
       <div className="container">
-        <div className="row">
-          <div className="col-sm-6">
-            <Card title="Free Package" linkText="Download Now">
-              <ul className="list-group text-left">
-                <li className="list-group-item">Unlimited Workouts</li>
-                <li className="list-group-item">3 Workout Templates</li>
-              </ul>
-            </Card>
-          </div>
-          <div className="col-sm-6">
-            <Card title="Goal Package" linkText="Download Now" subtitle='Only £5.00 a year'>
-              <ul className="list-group text-left">
-                <li className="list-group-item">Unlimited Workouts</li>
-                <li className="list-group-item">Unlimited Workout Templates</li>
-                <li className="list-group-item">Add Goals</li>
-                <li className="list-group-item">Progress Charts</li>
-                <li className="list-group-item">Suggested Sets</li>
-              </ul>
-            </Card>
-          </div>
-        </div>
+        <CardRow largeClass="col-lg-6">
+          <Card title="Free Package" linkText="Download Now">
+            <ul className="list-group text-left">
+              <li className="list-group-item">Unlimited Workouts</li>
+              <li className="list-group-item">3 Workout Templates</li>
+            </ul>
+          </Card>
+          <Card title="Goal Package" linkText="Download Now" subtitle='Only £5.00 a year'>
+            <ul className="list-group text-left">
+              <li className="list-group-item">Unlimited Workouts</li>
+              <li className="list-group-item">Unlimited Workout Templates</li>
+              <li className="list-group-item">Add Goals</li>
+              <li className="list-group-item">Progress Charts</li>
+              <li className="list-group-item">Suggested Sets</li>
+            </ul>
+          </Card>
+        </CardRow>
       </div>
     </div>
   )
