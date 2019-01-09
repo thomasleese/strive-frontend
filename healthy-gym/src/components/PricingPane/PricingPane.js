42a84f84542350521cd89d3890deb74fac02abd4
vikash tank <vikashtank@hotmail.co.uk>
2019-01-09 18:49:51 +0000

    updated pricing pane

--- healthy-gym/src/components/PricingPane/PricingPane.js
+++ healthy-gym/src/components/PricingPane/PricingPane.js
@@ -7,12 +7,14 @@ function Card(props) {
   return (
     <div className=' card border border-primary p-3 m-3 animate-in-down align-items-stretch align-content-stretch w-100'>
       <div className="card-body">
-        <h5 className="card-title">{props.title}</h5>
+        <h5 className="card-title text-center">{props.title}</h5>
         <span className='text-muted mb1'>{props.subtitle}</span>
         <div className="card-text py-3">
           {props.children}
         </div>
-        <a href="/" className="btn btn-primary">{props.linkText}</a>
+        <p className="text-center mt-4">
+          <a href="/" className="btn btn-primary">{props.linkText}</a>
+        </p>
       </div>
     </div>
   )
@@ -23,13 +25,15 @@ function PricingPane(props) {
     <div className="py-5 bg-primary" id="download">
       <div className="container">
         <CardRow largeClass="col-lg-6">
-          <Card title="Free Package" linkText="Download Now">
+          <Card title="Free" linkText="Download Soon">
+            <h5 className="lead text-center mb-4">Try free for a month with 30 workouts!</h5>
             <ul className="list-group text-left">
-              <li className="list-group-item">Unlimited Workouts</li>
+              <li className="list-group-item">30 Workouts</li>
               <li className="list-group-item">3 Workout Templates</li>
             </ul>
           </Card>
-          <Card title="Goal Package" linkText="Download Now" subtitle='Only £5.00 a year'>
+          <Card title="Pro" linkText="Download Soon">
+          <h5 className="lead text-center mb-4">Enjoy the best features, with a low price.</h5>
             <ul className="list-group text-left">
               <li className="list-group-item">Unlimited Workouts</li>
               <li className="list-group-item">Unlimited Workout Templates</li>
