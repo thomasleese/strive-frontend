c89574cacab752a49a9dee897b0b13b297a6cbbc
vikash tank <vikashtank@hotmail.co.uk>
2019-01-10 22:42:41 +0000

    align descriptions with improvement

--- healthy-gym/src/components/IntroPane/IntroPane.js
+++ healthy-gym/src/components/IntroPane/IntroPane.js
@@ -20,7 +20,7 @@ function IntroPane(props) {
           <div className="col-md-6 my-5 text-lg-left text-center align-start">
             <h1 className="display-3 text-light">Healthy Gym</h1>
             <h4 className="display-5 text-light">Your new favourite gym companion.</h4>
-            <p className="lead text-light">Join a growing community of gym goers who track their workouts using Healthy Gym.</p>
+            <p className="lead text-light">Join a growing community of gym goers who improve their workouts using Healthy Gym.</p>
             <div className="row mt-5">
               <div className="col-6 col-lg-4">
                 <IOSButton />
